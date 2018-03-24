7e1708af0b31063ff4be02dd40755921d76c015a
Daniel <dcabezas18@gmail.com>
2018-03-24 14:14:15 +0100

    First Commit

---
+++ src/Proxies.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel Cabezas
+ * Date: 24/03/2018
+ * Time: 13:08
+ */
+
+namespace dcabezas18\SimpleManagementProxies;
+
+use Tightenco\Collect\Support\Collection;
+
+class Proxies
+{
+    private $proxy;
+    private $proxies;
+    private $init;
+    private $end;
+    private $time;
+
+    /**
+     * Proxies constructor.
+     * @param $proxies
+     */
+    public function __construct($proxies)
+    {
+        $this->setProxies($proxies);
+    }
+
+    /**
+     * @return mixed
+     * @internal param $url
+     * @internal param array $options
+     */
+    public function get()
+    {
+        $this->proxy = $this->getProxyMinTime();
+        $this->end = microtime(true);
+        $this->getTimeToSave();
+        $this->setProxyTime($this->time);
+        $this->init = microtime(true);
+        return $this->proxy;
+    }
+
+    /**
+     * @param array $proxies
+     */
+    private function setProxies(array $proxies)
+    {
+        $this->proxies = collect($proxies);
+        foreach ($this->proxies as $proxy){
+            $proxy->time = 0;
+        }
+    }
+
+    private function getTimeToSave()
+    {
+        if(!empty($this->end) && !empty($this->init))
+            $this->time = $this->end - $this->init;
+        else $this->time = 0;
+    }
+    /**
+     * @param $time
+     */
+    private function setProxyTime($time)
+    {
+        foreach ($this->proxies as $pr){
+            if($pr->ip == $this->proxy->ip){
+                $pr->time = $pr->time + $time;
+            }
+        }
+    }
+
+    /**
+     * @return Collection
+     */
+    private function getProxyMinTime()
+    {
+        $minTime = $this->proxies->min('time');
+        return $this->proxies->where('time', $minTime)->first();
+    }
+}
---
+++ tests/ProxiesTests.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Daniel Cabezas
+ * Date: 24/03/2018
+ * Time: 13:02
+ */
+use dcabezas18\SimpleManagementProxies\Proxies;
+
+class ProxiesTests extends PHPUnit_Framework_TestCase
+{
+
+    public function dataProvider()
+    {
+        return [
+
+        ];
+    }
+}
